83af4e6ba2856e43171cb2aaaee6be3ee536379b
Sadeem Ahmed <asadeem065@gmail.com>
2026-06-12 13:55:39 +0500

    fix: update IAM policy with s3:ListBucket for instance role access

--- main.tf
+++ main.tf
@@ -137,9 +137,14 @@ resource "aws_iam_role_policy" "s3_and_ssm_access" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action   = ["s3:GetObject", "s3:ListBucket", "s3:PutObject", "s3:DeleteObject"]
+      Action   = ["s3:GetObject", "s3:ListBucket"]
       Effect   = "Allow"
-      Resource = ["${aws_s3_bucket.website_bucket.arn}", "${aws_s3_bucket.website_bucket.arn}/*", "arn:aws:s3:::${local.monitoring_bucket}", "arn:aws:s3:::${local.monitoring_bucket}/*"]
+      Resource = [
+        "${aws_s3_bucket.website_bucket.arn}",
+        "${aws_s3_bucket.website_bucket.arn}/*",
+        "arn:aws:s3:::${local.monitoring_bucket}",
+        "arn:aws:s3:::${local.monitoring_bucket}/*"
+      ]
     }]
   })
 }
